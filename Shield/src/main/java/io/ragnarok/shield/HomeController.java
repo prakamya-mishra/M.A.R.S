c43d2939d242595938fe07b75d9e4452d5af6c4f
arpanjain97 <arpanjain97@gmail.com>
2017-11-24 23:51:48 +0530

    Create Home Screen

---
+++ Shield/src/main/java/io/ragnarok/shield/HomeController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.ragnarok.shield;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+
+/**
+ * FXML Controller class
+ *
+ * @author Arpan
+ */
+public class HomeController implements Initializable {
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
