45f4aba3c0f7fec83279a5dab4596548e233f803
arpanjain97 <arpanjain97@gmail.com>
2017-11-24 23:49:55 +0530

    Connect Login and Sign Up through controller class

--- Shield/src/main/java/io/ragnarok/shield/FXMLController.java
+++ Shield/src/main/java/io/ragnarok/shield/FXMLController.java
@@ -5,17 +5,56 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField usernameFieldL;
+    @FXML
+    private PasswordField passwordFieldL;
+    @FXML
+    private TextField usernameFieldS;
+    @FXML
+    private PasswordField passwordFieldS;
+    @FXML
+    private PasswordField confirmPasswordFieldS;
+    
+    //Sign Up Screen
+    @FXML
+    private void handleSignUpAction(ActionEvent event) {
+        System.out.println(usernameFieldS.getText()+","+passwordFieldS.getText()+","+confirmPasswordFieldS.getText());
+    }
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleSignUpEnter(KeyEvent event){
+       if(event.getCode() == KeyCode.ENTER)
+        System.out.println(usernameFieldS.getText()+","+passwordFieldS.getText()+","+confirmPasswordFieldS.getText());
+    }
+    
+    //Login Screen
+    @FXML
+    private void handleLoginAction(ActionEvent event) {
+        System.out.println(usernameFieldL.getText()+" "+passwordFieldL.getText());
+    }
+    
+    @FXML
+    private void handleLoginEnter(KeyEvent event){
+       if(event.getCode() == KeyCode.ENTER)
+        System.out.println(usernameFieldL.getText()+" "+passwordFieldL.getText());
+    }
+    
+    //Launcher Screen
+    @FXML
+    private void handleLoginNavAction(ActionEvent event) {
+        System.out.println("Move to Login");
+    }
+    
+    @FXML
+    private void handleSignUpNavAction(ActionEvent event) {
+        System.out.println("move to Sign Up");
     }
     
     @Override
--- Shield/src/main/java/io/ragnarok/shield/MainApp.java
+++ Shield/src/main/java/io/ragnarok/shield/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
         
         Scene scene = new Scene(root,1366,768);
         scene.getStylesheets().add("/styles/Styles.css");
