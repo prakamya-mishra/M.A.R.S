64ea3684f06c0145978bf1d75d53eb4dd706bf77
rohanmittal73 <rohanmittal73@gmail.com>
2017-12-09 23:17:25 +0530

    Add authentication function

--- Shield/src/main/java/io/ragnarok/shield/Functions.java
+++ Shield/src/main/java/io/ragnarok/shield/Functions.java
@@ -1,6 +1,7 @@
 package io.ragnarok.shield;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -49,6 +50,60 @@ public class Functions {
         for (int i=0; i < 50 ; i++)
             songs_random_r.add(songs_random.get(i));
         return songs_random_r;
+    }
+
+    public static boolean authenticate(String username,String password)
+    {
+        Scanner scanner=null;
+        int flag=0;
+        try{
+
+            scanner = new Scanner(new File("User_db.csv"));
+            ArrayList<String> rewrite=new ArrayList<String>();
+            rewrite.add(scanner.nextLine());
+            while (scanner.hasNextLine())
+            {
+                String line=scanner.nextLine();
+                String[] columns = line.split(",");
+                if (columns[0].equals(username))
+                {
+                    if (columns[1].equals(password)) {
+                        line=columns[0]+","+columns[1]+","+"1"+","+columns[3]+","+columns[4]+","+columns[5]+","+columns[6]+","+columns[7]+","+columns[8]+","+columns[9]+","+columns[10]+","+columns[11]+","+columns[12]+","+columns[13]+"\n";
+                        rewrite.add(line);
+                        flag=1;
+                    }
+
+                    else {
+                        flag = 0;
+                        rewrite.add(line);
+                    }
+                }
+
+                else
+                {
+                    rewrite.add(line);
+                }
+
+            }
+
+            FileWriter writer=new FileWriter("User_db");
+            for(String s:rewrite)
+            {
+                writer.append(s);
+            }
+            writer.flush();
+            writer.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if (flag==1)
+        {
+            return true;
+        }
+        else
+            return false;
     }
 
 
