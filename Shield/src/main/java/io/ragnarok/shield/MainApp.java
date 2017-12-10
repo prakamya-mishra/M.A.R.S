f318172aacf1ce577ef19ae3855f171d855a5890
arpanjain97 <arpanjain97@gmail.com>
2017-12-10 08:33:28 +0530

    Add launcher screen

--- Shield/src/main/java/io/ragnarok/shield/MainApp.java
+++ Shield/src/main/java/io/ragnarok/shield/MainApp.java
@@ -3,21 +3,34 @@ package io.ragnarok.shield;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
 public class MainApp extends Application {
+    
+    static ObservableList<Node> childs;
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
+        Parent home = FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
+        Parent launch = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
         
-        Scene scene = new Scene(root,1366,768);
+        StackPane stack = new StackPane();
+        stack.getChildren().add(home);
+        stack.getChildren().add(launch);
+        MainApp.childs = stack.getChildren();
+        
+        Scene scene = new Scene(stack,1366,768);
         scene.getStylesheets().add("/styles/Styles.css");
         
         stage.setTitle("Shield");
@@ -26,6 +39,17 @@ public class MainApp extends Application {
         stage.minWidthProperty().set(650);
         stage.setScene(scene);
         stage.show();
+        FadeTransition ft = new FadeTransition(Duration.millis(500),launch);
+        ft.setFromValue(1.0);
+        ft.setToValue(0.0);
+        ft.setDelay(Duration.seconds(1));
+        ft.play();
+        ft.onFinishedProperty().set(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event){
+                MainApp.childs.get(MainApp.childs.size()-1).toBack();
+            }
+        });
     }
 
     /**
